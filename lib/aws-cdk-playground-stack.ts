3b6768a34af46b6962c238792946135f8f91d693
Lester John Eguaras <lesterjohneguaras@outlook.com>
2025-06-26 01:40:14 +0800

    Deployment parameters

--- lib/aws-cdk-playground-stack.ts
+++ lib/aws-cdk-playground-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { CfnOutput, Duration } from 'aws-cdk-lib';
+import { CfnOutput, CfnParameter, Duration } from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -24,9 +24,20 @@ export class AwsCdkPlaygroundStack extends cdk.Stack {
         }
     });
 
+    //Deployment parameters
+    //this is how to provide data to deployment using cloud formation parameter
+    //I mean this is how to specify things at the point when we are deploying
+    //eg. cdk deploy --parameter duration=7
+     const duration = new CfnParameter(this,'duration',{
+      default:6,
+      maxValue:10,
+      minValue:1,
+      type:'Number'
+    });
+
     //L2: Aws resources with higher=level - CDK provides additional fuctionality like defaults, boiler plate and type safety for many paramertes
     const myL2Bucket = new Bucket(this,'MyL2Bucket',{
-      lifecycleRules:[{expiration:Duration.days(2)}] 
+      lifecycleRules:[{expiration:Duration.days(duration.valueAsNumber)}] 
     });
     
     //this will not print information it will available only after deployed. 
@@ -35,6 +46,7 @@ export class AwsCdkPlaygroundStack extends cdk.Stack {
 
     //using cloud formation output it can find the information out right in console
     //CloudFormation outputs are a great way in which different cloudformation stacks can share information between them.
+   
     new CfnOutput(this,'MyL2BucketName',{
       value: myL2Bucket.bucketName
     })
