f28704bb2d285884f71092e16ae63796e09db36e
Lester John Eguaras <lesterjohneguaras@outlook.com>
2025-06-26 02:04:08 +0800

    CDK aspects : check or modify resources after they were created

--- bin/aws-cdk-playground.ts
+++ bin/aws-cdk-playground.ts
@@ -3,8 +3,11 @@ import * as cdk from 'aws-cdk-lib';
 import { AwsCdkPlaygroundStack } from './../lib/aws-cdk-playground-stack';
 import { PhotosStack } from '../lib/PhotosStack';
 import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+import { BucketTagger } from './bucket-tagger';
 
 const app = new cdk.App();
 new AwsCdkPlaygroundStack(app, 'AwsCdkPlaygroundStack', {});
 const photosStack =  new PhotosStack(app,'PhotosStack');
 new PhotosHandlerStack(app,'PhotosHandlerStack',{targetBucketArn:photosStack.photosBucketArn});
+const tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger);
