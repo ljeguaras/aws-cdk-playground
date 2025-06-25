1ad4753974a687d71e814e235664372e3a951e73
Lester John Eguaras <lesterjohneguaras@outlook.com>
2025-06-26 01:37:20 +0800

    Cloud formation output

--- lib/aws-cdk-playground-stack.ts
+++ lib/aws-cdk-playground-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { Duration } from 'aws-cdk-lib';
+import { CfnOutput, Duration } from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -25,10 +25,20 @@ export class AwsCdkPlaygroundStack extends cdk.Stack {
     });
 
     //L2: Aws resources with higher=level - CDK provides additional fuctionality like defaults, boiler plate and type safety for many paramertes
-    new Bucket(this,'MyL2Bucket',{
+    const myL2Bucket = new Bucket(this,'MyL2Bucket',{
       lifecycleRules:[{expiration:Duration.days(2)}] 
     });
     
+    //this will not print information it will available only after deployed. 
+    //because all this code happen locally.
+    console.log("bucket name:"+ myL2Bucket.bucketName);
+
+    //using cloud formation output it can find the information out right in console
+    //CloudFormation outputs are a great way in which different cloudformation stacks can share information between them.
+    new CfnOutput(this,'MyL2BucketName',{
+      value: myL2Bucket.bucketName
+    })
+
     //L3: Patterns: Combine multiple types of resources and helpt with common task in AWS. Ex. LambdaAPI
     new L3Bucket(this,'MyL3Bucket',5);
    
